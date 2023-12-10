f81573a143f784ee4488dbec782aa5fa9200041a
an1604 <nataf12386@gmail.com>
2023-12-10 20:09:55 +0200

    deploy deep learning model

--- DL_MODEL/DL_Model.py
+++ DL_MODEL/DL_Model.py
@@ -2,15 +2,14 @@ import os
 
 import numpy as np
 import pickle
-
-from tensorflow._api.v2.compat.v1 import keras
+import keras
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import re
 
 
 def load_DL_model():
     tokenizer_pickle_file = r'C:\Users\adina\PycharmProjects\pythonProject1\ML-deployment\tokenizer.pickle'
-    model_json_file =  r'C:\Users\adina\PycharmProjects\pythonProject1\ML-deployment\model.json'
+    model_json_file = r'C:\Users\adina\PycharmProjects\pythonProject1\ML-deployment\model.json'
 
     with open(tokenizer_pickle_file, 'rb') as tk:
         tokenizer = pickle.load(tk)
@@ -21,23 +20,19 @@ def load_DL_model():
 
     lstm_model = keras.models.model_from_json(loaded_model_json)
 
-    file_path = os.path.abspath("model.h5")
+    file_path = os.path.abspath(r'C:\Users\adina\PycharmProjects\pythonProject1\ML-deployment\model.h5')
     lstm_model.load_weights(file_path)
 
     return lstm_model, tokenizer
 
 
 def sentiment_prediction(review):
-    input_review = [review.lower()]
-    input_review = [re.sub('[^a-zA-z0-9\s]', '', x) for x in input_review]
-
     lstm_model, tokenizer = load_DL_model()
 
-    input_feature = tokenizer.texts_to_sequences(input_review)
-    input_feature = pad_sequences(input_feature, 1473, padding='pre')
+    test = tokenizer.texts_to_sequences([review.lower()])
+    test = pad_sequences(test, maxlen=30, dtype='int32', value=0)
 
-    sentiment = lstm_model.predict(input_feature)[0]
-
+    sentiment = lstm_model.predict(test)[0]
     if np.argmax(sentiment) == 0:
         pred = 'Negative'
     elif np.argmax(sentiment) == 1:
